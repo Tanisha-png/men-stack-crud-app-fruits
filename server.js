90c261d97dad73538035320225532c8dec45ccea
Tanisha <tthompsonjones8771@gmail.com>
2025-08-07 10:49:15 -0400

    Add fruit model

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
+
+const mongoose = require("mongoose");
 const express = require('express');
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
