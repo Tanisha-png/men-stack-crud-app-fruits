11b06aaff2a06cd75a2e381c52b19acf2636ec86
Tanisha <tthompsonjones8771@gmail.com>
2025-08-07 17:26:14 -0400

    Create form

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+const Fruit = require("./models/fruit.js");
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
@@ -21,3 +23,9 @@ app.listen(3000, () => {
 app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
+
+// GET /fruits/new
+app.get("/fruits/new", (req, res) => {
+    res.render("fruits/new.ejs");
+});
+
